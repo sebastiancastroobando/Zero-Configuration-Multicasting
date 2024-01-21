824860b580992d05a14f38768deca00a6b5a8296
Sebastian <sebastiancastroobando@gmail.com>
2024-01-21 08:42:04 -0500

    Added the provided starter code

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "zcs.h"
+
+void hello(char *s, char *r) {
+    printf("Ad received: %s, with value: %s\n", s, r);
+}
+
+int main() {
+    int rv;
+    rv = zcs_init();
+    char *names[10];
+    rv = zcs_query("type", "speaker", names, 10);
+    if (rv > 0) {
+        zcs_attribute_t attrs[5];
+        rv = zcs_get_attribs(names[0], attrs, 5);
+        if ((strcmp(attrs[0].attr_name, "location") == 0) &&
+            (strcmp(attrs[0].value, "kitchen") == 0)) {
+                rv = zces_listen_ad(names[0], hello);
+        }
+    }
+}
+
+

6fc463ff46137d11f64a2f72a9a511ab36021bb5
Denis <denisuzh@gmail.com>
2024-01-26 16:37:30 -0500

    Changes *int to int in get_attribs
    Signed-off-by: Denis <denisuzh@gmail.com>

--- zcs.h
+++ zcs.h
@@ -12,7 +12,7 @@ int zcs_init();
 int zcs_start(char *name, zcs_attribute_t attr[], int num);
 int zcs_post_ad(char *ad_name, char *ad_value);
 int zcs_query(char *attr_name, char *attr_value, char *node_names[]);
-int zcs_get_attribs(char *name, zcs_attribute_t attr[], int *num);
+int zcs_get_attribs(char *name, zcs_attribute_t attr[], int num);
 int zcs_listen_ad(char *name, zcs_cb_f cback);
 int zcs_shutdown();
 void zcs_log();

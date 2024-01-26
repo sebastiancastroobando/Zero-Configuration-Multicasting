22e09c1315ee7f8e0cb726c0c7b7b2133ec88a92
Denis <denisuzh@gmail.com>
2024-01-26 16:36:28 -0500

    Added header files and removed redunant args
    Signed-off-by: Denis <denisuzh@gmail.com>

--- app.c
+++ app.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include "zcs.h"
+#include <stdio.h>
+#include <string.h>
 
 void hello(char *s, char *r) {
     printf("Ad received: %s, with value: %s\n", s, r);
@@ -9,13 +11,13 @@ int main() {
     int rv;
     rv = zcs_init();
     char *names[10];
-    rv = zcs_query("type", "speaker", names, 10);
+    rv = zcs_query("type", "speaker", names);
     if (rv > 0) {
         zcs_attribute_t attrs[5];
         rv = zcs_get_attribs(names[0], attrs, 5);
         if ((strcmp(attrs[0].attr_name, "location") == 0) &&
             (strcmp(attrs[0].value, "kitchen") == 0)) {
-                rv = zces_listen_ad(names[0], hello);
+                rv = zcs_listen_ad(names[0], hello);
         }
     }
 }

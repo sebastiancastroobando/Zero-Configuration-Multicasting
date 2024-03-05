28eaea611e6bb111204d505fd93942fa4e30beb5
Sebastian <sebastiancastroobando@gmail.com>
2024-03-04 23:35:20 -0500

    Added structure for second and third part of demo

--- relay.c
+++ relay.c
@@ -2,6 +2,23 @@
 
 #include "multicast.h"
 
+int init_relay() {
+    // initialize the multicast groups
+    int rv;
+    rv = zcs_init(ZCS_APP_TYPE, channel1_LAN1, channel2_LAN1, port_LAN1);
+    if (rv < 0) {
+        printf("Error initializing ZCS for LAN1\n");
+        return -1;
+    }
+    rv = zcs_init(ZCS_APP_TYPE, channel1_LAN2, channel2_LAN2, port2_LAN2);
+    if (rv < 0) {
+        printf("Error initializing ZCS for LAN2\n");
+        return -1;
+    }
+    return 0;
+
+}
+
 // The relay is a bridge between two LANs. 
 int main() {
     // ask user for channel1, channel2, and port
@@ -28,5 +45,8 @@ int main() {
     scanf("%s", channel2_LAN2);
     printf("Enter port: ");
     scanf("%d", &port2_LAN2);
+
+
+    // initialize the multicast groups
 
 }

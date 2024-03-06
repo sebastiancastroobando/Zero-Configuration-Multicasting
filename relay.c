585d5ef19209f3c75f705a70d911de96e072527e
Sebastian <sebastiancastroobando@gmail.com>
2024-03-06 11:29:44 -0500

    setting up the multicast objects for the relay
    See relay_idea_sheme3.png for the logic

--- relay.c
+++ relay.c
@@ -2,17 +2,45 @@
 
 #include "multicast.h"
 
-mcast_t *mcast_LAN1_mrecv; // multicast Channel 1 for LAN1
-mcast_t *mcast_LAN1_msend; // multicast Channel 2 for LAN1
-mcast_t *mcast_LAN2_mrecv; // multicast Channel 1 for LAN2
-mcast_t *mcast_LAN2_msend; // multicast Channel 2 for LAN2
+// multicast groups for LAN1 and LAN2
+mcast_t *mcast_LAN1_CHANNEL1_mrecv; // multicast receiver Channel 1 for LAN1
+mcast_t *mcast_LAN1_CHANNEL1_msend; // multicast sender Channel 1 for LAN1
+mcast_t *mcast_LAN1_CHANNEL2_mrecv; // multicast receiver Channel 2 for LAN1
+mcast_t *mcast_LAN1_CHANNEL2_msend; // multicast sender Channel 2 for LAN1
+mcast_t *mcast_LAN2_CHANNEL1_mrecv; // multicast receiver Channel 1 for LAN2
+mcast_t *mcast_LAN2_CHANNEL1_msend; // multicast sender Channel 1 for LAN2
+mcast_t *mcast_LAN2_CHANNEL2_mrecv; // multicast receiver Channel 2 for LAN2
+mcast_t *mcast_LAN2_CHANNEL2_msend; // multicast sender Channel 2 for LAN2
 
-relay_init(char *channel1_LAN1, char *channel2_LAN1, int port_LAN1, char *channel1_LAN2, char *channel2_LAN2, int port2_LAN2) {
-    // initialize the multicast groups
-    mcast_init(channel1_LAN1, port_LAN1, port_LAN1 + 1);
-    mcast_init(channel2_LAN1, port_LAN1 - 1, port_LAN1);
-    mcast_init(channel1_LAN2, port2_LAN2, port2_LAN2 + 1);
-    mcast_init(channel2_LAN2, port2_LAN2 - 1, port2_LAN2);
+/**
+ * @details Ports are mirrored between channels in a given LAN, meaning :
+ * for an APP in LAN1, it would be :
+ * - msend : {channel1, port, port+1}; mrecv: {channel2, port-1, port}. 
+ * and for a SERVICE in LAN1, it would be:
+ * - msend : {channel2, port, port+1}; mrecv: {channel1, port-1, port}.
+ * 
+*/
+
+void reley_init(char *channel1_LAN1, char *channel2_LAN1, int port_LAN1, char *channel1_LAN2, char *channel2_LAN2, int port2_LAN2) {
+    // initialize the multicast groups for LAN1
+    mcast_LAN1_CHANNEL1_mrecv = mcast_init(channel1_LAN1, port_LAN1-1, port_LAN1);
+    mcast_LAN1_CHANNEL1_msend = mcast_init(channel1_LAN1, port_LAN1, port_LAN1+1);
+    mcast_LAN1_CHANNEL2_mrecv = mcast_init(channel2_LAN1, port_LAN1-1, port_LAN1);
+    mcast_LAN1_CHANNEL2_msend = mcast_init(channel2_LAN1, port_LAN1, port_LAN1+1);
+
+    // initialize the multicast groups for LAN2
+    mcast_LAN2_CHANNEL1_mrecv = mcast_init(channel1_LAN2, port2_LAN2-1, port2_LAN2);
+    mcast_LAN2_CHANNEL1_msend = mcast_init(channel1_LAN2, port2_LAN2, port2_LAN2+1);
+    mcast_LAN2_CHANNEL2_mrecv = mcast_init(channel2_LAN2, port2_LAN2-1, port2_LAN2);
+    mcast_LAN2_CHANNEL2_msend = mcast_init(channel2_LAN2, port2_LAN2, port2_LAN2+1);
+
+    // setup receiver multicast objects
+    mcast_setup_recv(mcast_LAN1_CHANNEL1_mrecv);
+    mcast_setup_recv(mcast_LAN1_CHANNEL2_mrecv);
+    mcast_setup_recv(mcast_LAN2_CHANNEL1_mrecv);
+    mcast_setup_recv(mcast_LAN2_CHANNEL2_mrecv);
+
+    
 }
 
 // The relay is a bridge between two LANs. 

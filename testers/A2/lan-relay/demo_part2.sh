94aa9e99fd6895c151902584f0b6568c93c62f51
Denis <denisuzh@gmail.com>
2024-03-12 23:16:56 -0400

    full testing
    Signed-off-by: Denis <denisuzh@gmail.com>

--- testers/A2/lan-relay/demo_part2.sh
+++ testers/A2/lan-relay/demo_part2.sh
@@ -18,9 +18,9 @@ rm -f logs/*
 ./LAN-B_service1 > logs/LAN-B_service1_log.txt 2>&1 &
 ./LAN-B_service2 > logs/LAN-B_service2_log.txt 2>&1 &
 ./LAN-A_app1 > logs/LAN-A_app1_log.txt 2>&1 &
-#./LAN-A_app2 > logs/LAN-A_app2_log.txt 2>&1 &
-#./LAN-B_app1 > logs/LAN-B_app1_log.txt 2>&1 &
-#./LAN-B_app2 > logs/LAN-B_app2_log.txt 2>&1 &
+./LAN-A_app2 > logs/LAN-A_app2_log.txt 2>&1 &
+./LAN-B_app1 > logs/LAN-B_app1_log.txt 2>&1 &
+./LAN-B_app2 > logs/LAN-B_app2_log.txt 2>&1 &
 
 # Wait for all background jobs to finish
 wait

0b8de00b03ec7b3232bcb1306a01f902ec846968
Denis <denisuzh@gmail.com>
2024-03-13 01:00:21 -0400

    adds script to run isolation and relay demos
    Signed-off-by: Denis <denisuzh@gmail.com>

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "RUNNING ISOLATION DEMO: ETA ~ 30sec"
+echo "demo is running in the background"
+
+cd testers/A2/lan-isolation/
+./demo-part1.sh
+
+if [[ ! -d ../../../isolation_logs ]]; then
+    mkdir ../../../isolation_logs
+fi
+
+cp logs/* ../../../isolation_logs/
+
+echo "FINISHED ISOLATION DEMO"
+echo "check contents of isolation_logs"
+
+echo "__________________________________"
+
+echo "RUNNING RELAY DEMO: ETA ~ 30sec"
+echo "demo is running in the background"
+
+cd ../lan-relay/
+./demo_part2.sh
+
+if [[ ! -d ../../../relay_logs ]]; then
+    mkdir ../../../relay_logs
+fi
+
+cp logs/* ../../../relay_logs/
+
+cd ../../../
+
+echo "FINISHED RELAY DEMO"
+echo "check contents of relay_logs"
